952da0b62e26e3ab751da8470adc1f67878c34a0
notify-ci2 <notify_ci34@yahoo.in>
2016-03-31 14:20:26 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var request = require('request');
 function trigger_build(project_id, callback) {
     var options = {
-        uri : 'https://apibeta.shippable.com/projects/56d2c43ec77dae78a8ed82ef/newbuild',
+        uri : 'https://apibeta.shippable.com/projects/56ef86b7c77dae78a8f945e7/newbuild',
         method : 'POST'
     }; 
     var res = '';
@@ -13,9 +13,9 @@ function trigger_build(project_id, callback) {
             res = 'Not Found';
         }
         callback(res);
-    });
+    });                  
 }
 
-trigger_build("56d2c43ec77dae78a8ed82ef", function(resp){
+trigger_build("56ef86b7c77dae78a8f945e7", function(resp){
     console.log(resp);
 });
